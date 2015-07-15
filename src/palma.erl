83536dbcc8c4a3aa6f5b18c191561a7ff9f66cde
odo <florian.odronitz@wooga.net>
2015-07-15 17:22:22 +0000

    - using supervisor2 for pools (see https://github.com/odo/supervisor2) - optional ReconnectDelay to palma:new/4 - bumped version to 0.3.0

--- src/palma.erl
+++ src/palma.erl
@@ -1,17 +1,20 @@
 -module(palma).
 
--export([start/0, new/3, pid/1]).
+-export([start/0, new/3, new/4, pid/1]).
 
 start() ->
     application:start(palma).
 
 new(PoolName, PoolSize, ChildSpec) ->
+    new(PoolName, PoolSize, ChildSpec, 10000).
+
+new(PoolName, PoolSize, ChildSpec, ReconnectDelay) ->
     {Id, {M, F, ChildInitArgs}, Restart, Shutdown, Type, Modules} = ChildSpec,
     ChildSpecNew     = {Id, {M, F, []}, Restart, Shutdown, Type, Modules},
     SupervisorName   = list_to_atom(atom_to_list(PoolName) ++ "_sup"),
     {ok, Supervisor} = supervisor:start_child(
         palma_sup,
-        {SupervisorName, {palma_pool_sup, start_link, [SupervisorName, ChildSpecNew]}, permanent, 10000, supervisor, [palma_pool_sup]}),
+        {SupervisorName, {palma_pool_sup, start_link, [SupervisorName, ChildSpecNew]}, permanent, ReconnectDelay, supervisor, [palma_pool_sup]}),
     [palma_pool_sup:start_child(Supervisor, ChildInitArgs) || _ <- lists:seq(1, PoolSize)],
     supervisor:start_child(
       palma_sup,
--- src/palma_pool_sup.erl
+++ src/palma_pool_sup.erl
@@ -1,14 +1,14 @@
 -module(palma_pool_sup).
--behaviour(supervisor).
+-behaviour(supervisor2).
 
 -export([start_link/2, init/1, start_child/2]).
 
 start_link(Id, Params) ->
-    supervisor:start_link({local, Id}, ?MODULE, Params).
+    supervisor2:start_link({local, Id}, ?MODULE, Params).
 
 start_child(Supervisor, Params) ->
-    supervisor:start_child(Supervisor, Params).
+    supervisor2:start_child(Supervisor, Params).
 
 init(ChildSpec) ->
-    {ok, {{simple_one_for_one, 10, 10}, [ChildSpec]}}.
+    {ok, {{simple_one_for_one, 1, 1}, [ChildSpec]}}.
 
