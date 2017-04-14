1e31e17fef92cc29bb1eafb45ce8df56d4846583
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2017-04-14 14:43:23 +0530

    ensure that we get pid

--- src/palma_pool_sup.erl
+++ src/palma_pool_sup.erl
@@ -4,10 +4,10 @@
 -export([start_link/2, init/1, start_child/2]).
 
 start_link(Id, Params) ->
-    palma_supervisor2:start_link({local, Id}, ?MODULE, Params).
+    {ok, _Pid} = palma_supervisor2:start_link({local, Id}, ?MODULE, Params).
 
 start_child(Supervisor, Params) ->
-    palma_supervisor2:start_child(Supervisor, Params).
+    {ok, _Pid} = palma_supervisor2:start_child(Supervisor, Params).
 
 init(ChildSpec) ->
     {ok, {{simple_one_for_one, 100, 1}, [ChildSpec]}}.
